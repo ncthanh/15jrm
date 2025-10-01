06cae35a79709167a3c5471036f6f3421a2a572f
Cong Nguyen <congthanh3806@gmail.com>
2025-09-30 21:20:15 -0700

    new stuff

--- src/Checkbox.jsx
+++ src/Checkbox.jsx
@@ -1,5 +1,5 @@
 export default function Checkbox ({ id, label }) {
-  return <div style={{backgroundColor: 'green', border: '1px solid red', borderRadius: '8px', padding: '10px', color: 'purple', fontSize: '40px'}}>
+  return <div style={{backgroundColor: 'green', border: '1px solid red', borderRadius: '8px', padding: '10px', color: 'orange', fontSize: '40px'}}>
     <input type="checkbox" id={id} style={{margin: '0 8px'}}/>
     <label for={id}>{label}</label>
   </div>
