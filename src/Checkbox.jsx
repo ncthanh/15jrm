0cf52cf16b8c54b279b5929282b435935038a1c3
Cong Nguyen <congthanh3806@gmail.com>
2025-09-30 19:57:57 -0700

    update
    skip-checks: true

--- src/Checkbox.jsx
+++ src/Checkbox.jsx
@@ -1,5 +1,5 @@
 export default function Checkbox ({ id, label }) {
-  return <div style={{backgroundColor: 'green', border: '1px solid red', padding: '10px'}}>
+  return <div style={{backgroundColor: 'green', border: '1px solid red', padding: '10px', color: 'yellow'}}>
     <input type="checkbox" id={id} style={{margin: '0 8px'}}/>
     <label for={id}>{label}</label>
   </div>
